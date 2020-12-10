a332e573b37b9ef0408498f8383c4f472c7d2b9a
chumicat <russell57260620@gmail.com>
2020-12-10 22:52:03 +0800

    finished

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+from multiprocessing import Process, Pool
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def print_num(num=0):
+    """ Print a specific number five times, each per 100ms
+
+    Args:
+        num (int): The specific number we want to print
+
+    Returns:
+        None: Result will print to standard output directly
+
+    """
+    for _ in range(5):
+        time.sleep(0.1)
+        print('{}'.format(num), end='')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # 1. First way: with process
+    processes = []
+    for i in range(1, 4):
+        p = Process(target=print_num, args=(i,))
+        processes.append(p)
+        p.start()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Join each process
+    for process in processes:
+        process.join()
+
+    print('')
+
+    # 2. Second way: with Pool
+    with Pool(3) as p:
+        p.map(print_num, [1, 2, 3])
